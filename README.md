ff388825f26d83892114177c88567372219f2c26
Ionică Bizău <bizauionica@gmail.com>
2015-09-01 18:36:44 +0300

    Installing node.js guide

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ My Raspberry Pi 2 story–progress, news, questions etc. :eyes: Watch the reposi
  - [1. Hardware](/story/1. Hardware.md)
  - [2. OS Installation](/story/2. OS Installation.md)
  - [3. LCD Screen Drivers Installation.md](/story/3. LCD Screen Drivers Installation.md)
+ - [4. Set up audio.md](4. Set up audio.md)
+ - [5. Installing Node.js](5. Installing Node.js)
---
+++ story/5. Installing Node.js
@@ -0,0 +1,17 @@
+# Installing Node.js
+
+Node.js installation is simple–I followed the [AdaFruit tutorial](https://learn.adafruit.com/node-embedded-development/installing-node-dot-js):
+
+```sh
+curl -sLS https://apt.adafruit.com/add | sudo bash
+sudo apt-get install node
+```
+
+Now you should have Node.js and NPM installed:
+
+```sh
+$ npm --version
+2.11.2
+$ node --version
+v0.12.6
+```
